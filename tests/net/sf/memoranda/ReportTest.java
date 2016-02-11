fb9a06056bc1811dd8056db2f4c338d0382075bf
James <jsmit106@asu.edu>
2016-02-11 15:30:46 -0700

    rewrite of ReportTest to no longer generate tasks and now verifies files are created from the html export.

--- tests/net/sf/memoranda/ReportTest.java
+++ tests/net/sf/memoranda/ReportTest.java
@@ -1,118 +1,91 @@
 package net.sf.memoranda;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
+import java.io.File;
 import java.util.Collection;
-import java.util.Random;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import net.sf.memoranda.CurrentProject;
-import net.sf.memoranda.ReportImpl;
-import net.sf.memoranda.Task;
-import net.sf.memoranda.TaskList;
-import net.sf.memoranda.date.CalendarDate;
 import net.sf.memoranda.util.FileStorage;
-import net.sf.memoranda.util.Util;
 
 public class ReportTest {
 
-	private static FileStorage store;
-	private ReportImpl report;
-	private Random rand;
-	private static TaskList taskList = CurrentProject.getTaskList();
-	private static ArrayList<String> allIds;
+	private Report report;
+	private Collection<Task> tasks;
+	private FileStorage store;
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		store = new FileStorage();
-		
-		for (int i = 0; i < 1000; i++) {
-			taskList.createTask(new CalendarDate(2016, 1, 1), new CalendarDate(2016, 1, 30), String.format("Task %d", i), "Foo", 3, (long)2.5, "blah", null);
-		}
-
-		store.storeTaskList(taskList, CurrentProject.get());
-		
-		allIds = (ArrayList<String>)(CurrentProject.getTaskList().getIds());
-	}
-
 	@Before
-	public void setUp() throws Exception {
-		report = new ReportImpl();
-		rand = new Random();
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		report = null;
-		rand = null;
-	}
-	
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		// Delete tasks
-
-		for (int i = 0; i < 1000; i++) {
-			taskList.removeTask(taskList.getTask(allIds.get(i)));
+	public void before() {
+		store = new FileStorage();
+		tasks = CurrentProject.getTaskList().getTopLevelTasks();
+		String[] ids = new String[tasks.size()];
+		int i = 0;
+		
+		for (Task t : tasks) {
+			ids[i] = t.getID();
+			i++;
 		}
-
-		store.storeTaskList(taskList, CurrentProject.get());
 		
-		taskList = null;
-		store = null;
+		report = new ReportImpl();
+		report.setTasks(ids);
 		
+		File file = new File(store.getReportPath());
+		if (file.exists()) {
+			file.delete();
+		}
 	}
-
-	@Test
-	public void testGetStyle() {
-		report.setStyle(3);
-		assertEquals(Integer.valueOf(0), Integer.valueOf(report.getStyle()));
-		
-		report.setStyle(2);
-		assertEquals(Integer.valueOf(2), Integer.valueOf(report.getStyle()));
 	
-		report.setStyle(-1);
-		assertEquals(Integer.valueOf(0), Integer.valueOf(report.getStyle()));
+	@Test
+	public void testGetSetStyle() {
+		report.setStyle(Report.STYLE_MINIMUM);
+		assertEquals(Report.STYLE_MINIMUM, report.getStyle(), 0);
+		report.setStyle(Report.STYLE_MEDIUM);
+		assertEquals(Report.STYLE_MEDIUM, report.getStyle(), 0);
+		report.setStyle(Report.STYLE_MAXIMUM);
+		assertEquals(Report.STYLE_MAXIMUM, report.getStyle(), 0);
 	}
-
+	
 	@Test
 	public void testGetTasks() {
+		Collection<Task> gotTasks = report.getTasks();
 		
-		Collection<String> taskIds = CurrentProject.getTaskList().getIds();
-
-		int size = rand.nextInt(1000); // get number from 0-999
-		
-		String[] someIds = new String[size];
-		
-		for (int i = 0; i < size; i++) {
-			someIds[i] = allIds.get(i);
-		}
-		
-		report.setTasks(someIds);
-		
-		Collection<Task> tasks = report.getTasks();
+		assertEquals(tasks, gotTasks);
 	}
 	
 	@Test
 	public void testExportHtmlMin() {
-		report.setStyle(0);
+		report.setStyle(Report.STYLE_MINIMUM);
 		report.exportHtml();
+		
+		File file = new File(store.getReportPath());
+		assertTrue(file.exists());
+		file.delete();
+		assertFalse(file.exists());
 	}
 	
 	@Test
 	public void testExportHtmlMed() {
-		report.setStyle(1);
+		report.setStyle(Report.STYLE_MEDIUM);
 		report.exportHtml();
+		
+		File file = new File(store.getReportPath());
+		assertTrue(file.exists());
+		file.delete();
+		assertFalse(file.exists());
 	}
 	
 	@Test
 	public void testExportHtmlMax() {
-		report.setStyle(2);
+		report.setStyle(Report.STYLE_MAXIMUM);
 		report.exportHtml();
+		
+		File file = new File(store.getReportPath());
+		assertTrue(file.exists());
+		file.delete();
+		assertFalse(file.exists());
 	}
 }
